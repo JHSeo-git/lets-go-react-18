ebf6633133fcc99a023f47afbe70a0de863f8988
JHSeo <qnfqnfqnf@gmail.com>
2022-03-31 03:26:00 +0900

    update AutomaticBatching page

--- src/components/AutomaticBatching/AutomaticBatching.helpers.ts
+++
@@ -1,3 +0,0 @@
-export function fetchSomthing(delay = 100) {
-  return new Promise((resolve) => setTimeout(resolve, delay));
-}
--- src/components/AutomaticBatching/AutomaticBatching.tsx
+++ src/components/AutomaticBatching/AutomaticBatching.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
+import { fetchSomthing } from '../../helpers/common.helpers';
 import LogEvents from '../LogEvents';
-import { fetchSomthing } from './AutomaticBatching.helpers';
 
 function AutomaticBatching() {
   const [count, setCount] = useState(0);
@@ -14,14 +14,29 @@ function AutomaticBatching() {
   };
 
   return (
-    <div>
-      <h2>AutomaticBatching</h2>
-      <div>
+    <article>
+      <h2>Automatic Batching</h2>
+      <div
+        style={{
+          marginTop: '1rem',
+          display: 'flex',
+          alignItems: 'center',
+          gap: '1rem',
+        }}
+      >
         <button onClick={handleClick}>Next</button>
-        <p style={{ color: flag ? 'blue' : 'black' }}>{count}</p>
+        <p
+          style={{
+            fontSize: '2rem',
+            fontWeight: 'bold',
+            color: flag ? 'blue' : 'red',
+          }}
+        >
+          {count}
+        </p>
       </div>
       <LogEvents />
-    </div>
+    </article>
   );
 }
 
