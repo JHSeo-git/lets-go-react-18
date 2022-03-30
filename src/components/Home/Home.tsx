e3cc427f5441499e880324c0b076aa840e77b81a
JHSeo <qnfqnfqnf@gmail.com>
2022-03-31 03:26:24 +0900

    update Home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,13 @@
 function Home() {
   return (
-    <div style={{ marginTop: '2rem' }}>
+    <article>
       <h2>Home</h2>
       <p>React 18 is released!</p>
       <ol>
         <li>Automatic Batching</li>
         <li>Transitions</li>
       </ol>
-    </div>
+    </article>
   );
 }
 
