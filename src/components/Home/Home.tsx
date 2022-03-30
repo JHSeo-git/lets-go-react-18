00a8e4d1dbe9332f4df5d9ee9292971ac7cc32ae
JHSeo <qnfqnfqnf@gmail.com>
2022-03-30 18:45:39 +0900

    add Home component

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div style={{ marginTop: '2rem' }}>
+      <h2>Home</h2>
+      <p>React 18 is released!</p>
+      <ol>
+        <li>Automatic Batching</li>
+        <li>Transitions</li>
+      </ol>
+    </div>
+  );
+}
+
+export default Home;
