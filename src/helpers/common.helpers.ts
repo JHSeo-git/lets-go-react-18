71d5f8bf017ba4742a91ef477cced02ce6c1bc03
JHSeo <qnfqnfqnf@gmail.com>
2022-03-31 23:49:19 +0900

    add fetchData helper

--- src/helpers/common.helpers.ts
+++ src/helpers/common.helpers.ts
@@ -1,3 +1,19 @@
 export function fetchSomthing(delay = 100) {
   return new Promise((resolve) => setTimeout(resolve, delay));
 }
+
+type IdType = number;
+type FetchType = `item/${IdType}` | 'topstories';
+
+export async function fetchData(type: FetchType, delay = 0) {
+  const [res] = await Promise.all([
+    fetch(`https://hacker-news.firebaseio.com/v0/${type}.json`),
+    new Promise((resolve) => setTimeout(resolve, delay)),
+  ]);
+
+  if (res.status !== 200) {
+    throw new Error(`Status code: ${res.status}`);
+  }
+
+  return res.json();
+}
