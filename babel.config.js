8fd80316e7e646ad25b4524ec6c1b658b1f2e7b8
JHSeo <qnfqnfqnf@gmail.com>
2022-04-02 22:27:16 +0900

    chore add babel plugin
    - @babel/plugin-transform-runtime

--- babel.config.js
+++ babel.config.js
@@ -24,6 +24,6 @@ module.exports = (api) => {
         },
       ],
     ],
-    plugins: ['@loadable/babel-plugin'],
+    plugins: ['@loadable/babel-plugin', '@babel/plugin-transform-runtime'],
   };
 };
