16584c543b231177819dd183e91c3f73639b0f17
JHSeo <qnfqnfqnf@gmail.com>
2022-04-03 18:59:20 +0900

    add assetParser helper

---
+++ src/server/lib/assetParser.tsx
@@ -0,0 +1,34 @@
+type AssetsManifest = {
+  files: {
+    [key: string]: string;
+  };
+  entrypoints: string[];
+};
+
+export const assetsParser = (manifest: AssetsManifest) => {
+  const { files, entrypoints } = manifest;
+
+  const getLinkElements = () =>
+    entrypoints.map((key) => {
+      const href = files[key];
+      const as = key.endsWith('.css') ? 'style' : 'script';
+
+      return <link key={key} rel="preload" href={href} as={as} />;
+    });
+
+  const getStyleElements = () =>
+    Object.keys(files)
+      .filter((key) => key.endsWith('.css') && !key.endsWith('.module.css'))
+      .map((key) => {
+        return <link key={key} rel="stylesheet" href={files[key]} />;
+      });
+
+  const getScriptElements = () =>
+    Object.keys(files)
+      .filter((key) => key.endsWith('.js') && !key.endsWith('.module.js'))
+      .map((key) => {
+        return <script key={key} async src={files[key]}></script>;
+      });
+
+  return { getLinkElements, getStyleElements, getScriptElements };
+};
