9f104a39e163051d001334322cf0d1257661c0fc
JHSeo <qnfqnfqnf@gmail.com>
2022-04-04 07:22:26 +0900

    add condition for dev hmr

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -9,13 +9,20 @@ const container = document.getElementById('app');
 if (!container) {
   throw new Error('"app" element not found');
 }
-
-ReactDOM.hydrateRoot(
-  container,
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+if (process.env.NODE_ENV === 'production') {
+  ReactDOM.hydrateRoot(
+    container,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+} else {
+  ReactDOM.createRoot(container).render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
 
 if (module.hot) {
   module.hot.accept();
